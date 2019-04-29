2739c4704344921399a08837dbf898003e91b78c
barrylee111 <barrylee115@gmail.com>
2019-04-29 16:53:57 -0600

    Updates

---
+++ precache-manifest.81498ad206059fe5e33665ad1d7e3fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fec6d6023ac4d28f67c29411825994",
+    "url": "/tomis/index.html"
+  },
+  {
+    "revision": "3b353751403c950cab96",
+    "url": "/tomis/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/tomis/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3b353751403c950cab96",
+    "url": "/tomis/static/js/main.b8282ced.chunk.js"
+  },
+  {
+    "revision": "a0807d6df37c52033865",
+    "url": "/tomis/static/js/runtime~main.d24df16a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tomis/static/media/logo.5d5d9eef.svg"
+  }
+]);
