80602efab19a4f090129841eca25aa6008f6679e
barrylee111 <barrylee115@gmail.com>
2019-05-01 17:17:19 -0600

    M

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class App extends Component {
     document.body.style.backgroundColor="#E0F4F4";
 
     return (
-      <div className="flex-col asc">
-        <div style={{background: "#2B3030", alignSelf: "middle"}} className="shadow-5 flex justify-between ml6 mr6">
+      <div className="mt6 flex-col asc">
+        <div style={{background: "#2B3030"}} className="shadow-5 flex justify-between ml6 mr6 pt3 pb3">
           
             <Unit />
             
